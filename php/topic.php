7296622562efe0e7d52bef448ad37d66425be513
omgzui <862275679@qq.com>
2017-11-09 21:54:22 +0800

    面试题

---
+++ php/topic.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shengj
+ * Date: 2017/11/9
+ * Time: 21:53
+ */
+
+//- 1、mysql事务的并行，锁问题
+//- 2、mysql论坛表设计
+//- 3、设计模式运用
+//- 4、php代码解释
+//- 5、百度统计的实现原理
+//- 6、自己最满意的代码
+//- 7、共享session，丢失怎么办
+//- 8、git流程（master需要上一个bug_fix分支）
+//- 9、app和pc端的处理不同？
