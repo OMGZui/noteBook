56ad89ba775ed4bef9a1c56aadba5537eea71038
omgzui <862275679@qq.com>
2017-12-11 16:30:49 +0800

    container

--- php/container/src/container.php
+++ php/container/src/container.php
@@ -5,3 +5,28 @@
  * Date: 2017/12/9
  * Time: 12:25
  */
+
+namespace Shengj\Container;
+
+use Illuminate\Container\Container;
+
+require __DIR__.'/../vendor/autoload.php';
+
+//实例化容器
+$container = new Container();
+
+//全局绑定事件
+//$container->resolving(function ($object, $app) {
+//    dump($object->echo());
+//});
+
+//绑定
+$container->bind('hello',Hello::class);
+
+//解析
+$hello = $container->make('hello');
+dump($hello->echo());
+dump($container->has('hello'));
+dump($container);
+
+
--- php/container/src/hello.php
+++ php/container/src/hello.php
@@ -5,3 +5,16 @@
  * Date: 2017/12/9
  * Time: 12:32
  */
+
+namespace Shengj\Container;
+
+class Hello
+{
+    public function __construct(){}
+
+    public function echo()
+    {
+        return 'hello';
+    }
+
+}
