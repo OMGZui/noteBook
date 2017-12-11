4ce28071fe2969b23c4ace900cdcc015d64e81ba
omgzui <862275679@qq.com>
2017-12-11 21:29:46 +0800

    main

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
  * @Author: 小粽子 
  * @Date: 2017-12-02 21:39:29 
  * @Last Modified by: 小粽子
- * @Last Modified time: 2017-12-11 19:28:20
+ * @Last Modified time: 2017-12-11 21:29:35
  */
 
 var express = require('express');
 var app = express();
 
 
-app.use(express.static('dist'));
+app.use(express.static('public'));
 
 app.listen(5000, function () {
     console.log('port 5000!');
